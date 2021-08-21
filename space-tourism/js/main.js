b3f24a75d44a930ebb37c9dcbe3543c54960a76b
böris <boris.yarmoulnik@gmail.com>
2021-08-21 17:13:20 +0300

    feat: finish space tourism responsive

--- space-tourism/js/main.js
+++ space-tourism/js/main.js
@@ -3,6 +3,14 @@ $(function(){
   $(".slider__box").slick({
     prevArrow: '<img class="slider__arrow slider__arrow-left" src="./images/arrow-left.svg" alt="">',
     nextArrow: '<img class="slider__arrow slider__arrow-right" src="./images/arrow-right.svg" alt="">',
+    responsive: [
+      {
+        breakpoint: 481,
+        settings: {
+          arrows: false,
+        }
+      }
+    ]
   });
 
   $(".menu__button").on("click", function() {
